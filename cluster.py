b890798377084c48caef4bf5a429c798002cda7d
ecenazcalkinsin <162919203+ecenazcalkinsin@users.noreply.github.com>
2024-10-17 09:13:25 +0300

    Add files via upload

---
+++ cluster.py
@@ -0,0 +1,17 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def run_cluster(df: pd.DataFrame, k: int):
+    """
+    Perform k-means clustering on the given DataFrame.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame.
+    k (int): The number of clusters to create.
+    """
+    kmeans = KMeans(n_clusters=k, random_state=0)
+    kmeans.fit(df.drop("name", axis=1))
+    df["Cluster"] = kmeans.labels_
+    return df
